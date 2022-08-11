bffac771b3b657d7356310df5a0fa19ef90217f9
porfinogeneta <szyman123drum@gmail.com>
2022-08-11 10:42:46 +0200

    added files

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', ( req, res ) => {
 
     (async () => {
         // const browser = await puppeteer.launch({headless: false})
-        const browser = await puppeteer.launch()
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox']})
         const page = await browser.newPage()
         await page.goto('https://www.diki.pl/dictionary/last-searches-de')
         // redirect to login
